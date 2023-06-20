ec4917f5a0572369b3bf519351fb74d63ac15a73
Diego Santa Cruz <gonzalo.santacruzm@gmail.com>
2023-06-20 14:05:17 -0700

    feat: newscards

--- src/Components/NewsCard.jsx
+++ src/Components/NewsCard.jsx
@@ -1,23 +1,41 @@
-import { Button, Card } from "antd";
+import { Link } from "react-router-dom";
 
-export default function NewsCard() {
+export default function NewsCard({ redirectUrl, title, message }) {
   return (
-    <Card style={styles.container}>
-      <img src="https://picsum.photos/300/200" alt="" />
-      <h3>
-        Introducing the SLA 750, the Next Revolution in Stereolithography (SLA)
-      </h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur,
-        assumenda? Neque doloribus eos iusto quidem architecto tempore,
-        voluptate repudiandae molestiae saepe dolore atque ipsam corporis minima
-        laudantium sint at. Fugiat.
-      </p>
-      <Button>Leer mas...</Button>
-    </Card>
+    <Link to={redirectUrl} style={styles.linkContainer}>
+      <div style={styles.container}>
+        <img src="https://picsum.photos/800/400" alt="" style={styles.image}/>
+        <div style={styles.infoContainer}>
+          <h3>
+            {title}
+          </h3>
+          <p>
+            {message}
+          </p>
+        </div>
+      </div>
+    </Link>
   );
 }
 
 const styles = {
-  container: {},
+  linkContainer: {
+    flexGrow: 1,
+    border: '1px solid rgb(200, 200, 200)',
+    borderRadius: 8,
+    width: '100%',
+  }, 
+  container: {
+
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    objectFit: 'cover',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  infoContainer: {
+    padding: 10,
+  },
 };
