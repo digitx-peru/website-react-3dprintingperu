b8f5733704ebbb483e47419b0f5e01801ce71123
Diego Santa Cruz <gonzalo.santacruzm@gmail.com>
2023-07-29 22:23:11 -0700

    update: printerCard has now all dynamic fields

--- src/Components/PrinterScreen/PrinterCard.jsx
+++ src/Components/PrinterScreen/PrinterCard.jsx
@@ -1,16 +1,18 @@
 import useMediaQuery from "../../hooks/useMediaQuery";
 
-export default function PrinterCard({ name, description, software, price }) {
-  
-  
-  const isDevice = useMediaQuery(768);
-
-  const isColumn = useMediaQuery(1024)
+export default function PrinterCard({
+  printerImageUrl,
+  name,
+  description,
+  builVolume,
+  technology,
+}) {
+  const isColumn = useMediaQuery(1024);
 
   const styles = {
     printerCardContent: {
       border: "1px solid silver",
-      borderRadius: 8,
+      borderRadius: "8px",
       width: isColumn ? "100%" : "300px",
     },
     printerCardImage: {
@@ -21,33 +23,47 @@ export default function PrinterCard({ name, description, software, price }) {
     printerCardInfoContent: {
       padding: "10px",
     },
+    printerName: {
+      fontSize: "24px",
+      margin: "10px 0px",
+    },
     printCardDescription: {
       fontSize: isColumn ? "14px" : "16px",
-      minHeight: isColumn ? "75px" : "100px"
+      minHeight: isColumn ? "75px" : "100px",
+    },
+    printerVolumeAndTechnologyWrapper: {
+      display: "flex",
+      flex: 1,
+      flexDirection: "row",
+      gap: "20px",
     },
     printCardBodyText: {
       fontSize: isColumn ? "14px" : "16px",
-    }
+    },
   };
   return (
     <div style={styles.printerCardContent}>
       <img
-        src="https://picsum.photos/300/150"
-        alt=""
+        src={printerImageUrl}
+        alt={name + " 3d printer"}
         style={styles.printerCardImage}
       />
       <div style={styles.printerCardInfoContent}>
-        <h4>{name}</h4>
+        <h4 style={styles.printerName}>{name}</h4>
         <p style={styles.printCardDescription}>{description}</p>
         <div style={{ display: "flex", flexDirection: "row", gap: 50 }}>
-          <div>
-            <h4>Volume XYZ (mm)</h4>
-            <p style={styles.printCardBodyText}>100x100x100</p>
+          <div style={styles.printerVolumeAndTechnologyWrapper}>
+            <div>
+              <h4>Volumen XYZ ({builVolume.unit})</h4>
+              <p style={styles.printCardBodyText}>
+                {builVolume.x} x {builVolume.y} x {builVolume.z}
+              </p>
+            </div>
+            <div>
+              <h4>Tecnología</h4>
+              <p style={styles.printCardBodyText}>{technology}</p>
+            </div>
           </div>
-          {/* <div>
-            <h4>Precio Ref</h4>
-            <p style={styles.printCardBodyText}>{price}</p>
-          </div> */}
         </div>
       </div>
     </div>
