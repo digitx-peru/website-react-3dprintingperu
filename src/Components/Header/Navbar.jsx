32ee80d000211add60ef1dcd4529c1d85a16fbb7
Diego Santa Cruz <gonzalo.santacruzm@gmail.com>
2023-10-02 22:58:38 -0700

    update: correct link on navbar

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -7,71 +7,62 @@ import { Dropdown, Space } from "antd";
 const itemsIndustrias = [
   {
     key: "1",
-    label: <Link to="/industrias/automotriz">Automotriz</Link>,
-  },
-  {
-    key: "2",
     label: <Link to="/industrias/fundicion">Fundicion</Link>,
   },
   {
-    key: "3",
+    key: "2",
     label: <Link to="/industrias/joyeria">Joyeria</Link>,
   },
   {
     key: "4",
-    label: <Link to="/industrias/replicas">Replicas</Link>,
+    label: <Link to="/industrias/arte y proto">Arte y prototipos</Link>,
   },
 ];
 
 const itemsSalud = [
   {
     key: "1",
-    label: <Link to="/salud/medicina">Medicina</Link>,
+    label: <Link to="/salud/medicina">Dispositivos Médicos</Link>,
   },
   {
     key: "2",
-    label: <Link to="/salud/odontologia">Odontologia</Link>,
+    label: <Link to="/salud/odontologia">Bioprinting</Link>,
   },
 ];
 
 const itemsProductos = [
   {
     key: "1",
-    label: <Link to="/productos/auxiliares">Auxiliares</Link>,
-    disabled: true,
-  },
-  {
-    key: "2",
     label: <Link to="/productos/impresoras">Impresoras</Link>,
   },
   {
-    key: "3",
+    key: "2",
     label: <Link to="/productos/materiales">Materiales</Link>,
   },
 ];
 
-const itemsServicios = [
-  {
-    key: "1",
-    label: <Link to="/aplicaciones/automotriz">Automotriz</Link>,
-    disabled: true,
-  },
-  {
-    key: "2",
-    label: <Link to="/aplicaciones/fundicion">Fundicion</Link>,
-    disabled: true,
-  },
-  {
-    key: "3",
-    label: <Link to="/aplicaciones/joyeria">Joyeria</Link>,
-    disabled: true,
-  },
-  {
-    key: "4",
-    label: <Link to="/aplicaciones/replicas">Replicas</Link>,
-    disabled: true,
-  },
-];
+// const itemsServicios = [
+//   {
+//     key: "1",
+//     label: <Link to="/aplicaciones/automotriz">Automotriz</Link>,
+//     disabled: true,
+//   },
+//   {
+//     key: "2",
+//     label: <Link to="/aplicaciones/fundicion">Fundicion</Link>,
+//     disabled: true,
+//   },
+//   {
+//     key: "3",
+//     label: <Link to="/aplicaciones/joyeria">Joyeria</Link>,
+//     disabled: true,
+//   },
+//   {
+//     key: "4",
+//     label: <Link to="/aplicaciones/replicas">Replicas</Link>,
+//     disabled: true,
+//   },
+// ];
 
 ////////////////////////// Main Component //////////////////////////
 
@@ -109,7 +100,7 @@ export default function Navbar() {
         </a>
       </Dropdown>
 
-      <Dropdown
+      {/* <Dropdown
         menu={{
           items: itemsServicios,
         }}
@@ -118,7 +109,7 @@ export default function Navbar() {
         <a onClick={(e) => e.preventDefault()}>
           <Space style={styles.dropDownText}>Servicios</Space>
         </a>
-      </Dropdown>
+      </Dropdown> */}
     </nav>
   );
 }
