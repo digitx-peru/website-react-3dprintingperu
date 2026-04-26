4bb2e3e069be28bb2daaf3068493c384d2250198
Diego Santa Cruz <gonzalo.santacruzm@gmail.com>
2026-04-26 00:57:40 -0700

    update: infocard

--- src/Components/IconInfoCard.jsx
+++ src/Components/IconInfoCard.jsx
@@ -1,32 +1,34 @@
 export default function PageContent({ iconImage, title, message }) {
-  // const isDevice = useMediaQuery(768);
-  // const is1440 = useMediaQuery(1440);
-
-  // const styles = {
-  //   cardContainer: {
-  //     display: "flex",
-  //     flexDirection: "column",
-  //     alignItems: "center",
-  //     //   rowGap: "0px",
-  //   },
-  //   pageContent: {
-  //     display: "flex",
-  //     flexDirection: "column",
-  //     justifyContent: "center",
-  //     padding: "50px 0px",
-  //     width: "100vw",
-  //     backgroundColor: "rgb(239,251,246)",
-  //   },
-  //   column: {
-  //     paddingLeft: 100,
-  //     paddingRight: 100,
-  //   },
-  // };
+  const styles = {
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      width: "300px",
+      height: "400px",
+      backgroundColor: "#D9D9D8",
+      padding: "15px",
+      boxSizing: "border-box",
+    },
+    image: {
+      alignSelf: "flex-end",
+      marginBottom: "15px",
+    },
+    title: {
+      marginBottom: "10px",
+    },
+  };
 
   return (
-    <div className="cardContainer" style={{width: "400px", height: "300px", backgroundColor: "#D9D9D8"}}>
-      <img src="" alt="" className="icon" />
-      <h2 className="cardTitle">{title}</h2>
+    <div className="cardContainer" style={styles.container}>
+      <img
+        src="\images\infocard\info_card_icon_concept.png"
+        alt=""
+        className="icon"
+        style={styles.image}
+      />
+      <h2 className="cardTitle" style={styles.title}>
+        {title}
+      </h2>
       <p className="cardInfo">{message}</p>
     </div>
   );
--- src/Components/TitleMessageInfocard.jsx
+++ src/Components/TitleMessageInfocard.jsx
@@ -4,12 +4,32 @@ export default function TitleMessageInfocard({
   message,
   sectionName,
 }) {
-
   return (
-    <section className={sectionName} style={{display: "flex", flexDirection: "column", alignItems: "stretch", padding: "0px 200px"}}>
-      <h2 className="title">{title}</h2>
-      <p className="message">{message}</p>
-      <div className="IconInfoCardContainer" style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>{children}</div>
+    <section
+      className={sectionName}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        padding: "0px 200px",
+        margin: "40px 0",
+      }}
+    >
+      <h2 className="title" style={{ marginBottom: "20px" }}>
+        {title}
+      </h2>
+      <p className="message" style={{ marginBottom: "20px" }}>{message}</p>
+      <div
+        className="IconInfoCardContainer"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: "40px",
+        }}
+      >
+        {children}
+      </div>
     </section>
   );
 }
